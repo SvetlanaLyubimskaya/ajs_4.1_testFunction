3318eddf348978f89a6593a19106ca5657c2bae0
SvetlanaLyubimskaya <lyubimskaya.s@yandex.ru>
2021-03-16 19:59:24 +0300

    eslint fix

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,32 +1,32 @@
 import getHealth from '../app';
 
 test('health status 1', () => {
-    const result = getHealth({ name: 'Маг', health: 90 });
-    // const received = getHealth(obj);
+  const result = getHealth({ name: 'Маг', health: 90 });
+  // const received = getHealth(obj);
 
-    expect(result).toBe('healthy');
+  expect(result).toBe('healthy');
 });
 
 test('health status 2', () => {
-    const result = getHealth({ name: 'Маг', health: 50 });
-    
-    expect(result).toBe('healthy');
+  const result = getHealth({ name: 'Маг', health: 50 });
+
+  expect(result).toBe('healthy');
 });
 
 test('health status 3', () => {
-    const result = getHealth({ name: 'Маг', health: 20 });
+  const result = getHealth({ name: 'Маг', health: 20 });
 
-    expect(result).toBe('wounded');
+  expect(result).toBe('wounded');
 });
 
 test('health status 4', () => {
-    const result = getHealth({ name: 'Маг', health: 15 });
+  const result = getHealth({ name: 'Маг', health: 15 });
 
-    expect(result).toBe('wounded');
+  expect(result).toBe('wounded');
 });
 
 test('health status 5', () => {
-    const result = getHealth({ name: 'Маг', health: 10 });
+  const result = getHealth({ name: 'Маг', health: 10 });
 
-    expect(result).toBe('critical');
+  expect(result).toBe('critical');
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,11 @@
 export default function getHealth(obj) {
-    let result;
-    if (obj.health >= 50) {
-        result = 'healthy';
-    } else if (obj.health < 50 && obj.health >= 15) {
-        result = 'wounded';
-    } else if (obj.health < 15) {
-        result = 'critical';
-    }
-    return result;
-} 
+  let result;
+  if (obj.health >= 50) {
+    result = 'healthy';
+  } else if (obj.health < 50 && obj.health >= 15) {
+    result = 'wounded';
+  } else if (obj.health < 15) {
+    result = 'critical';
+  }
+  return result;
+}
